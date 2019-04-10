6f341a7e32e00ca361723005109dcec970202ba2
Ryuhei Shida <ryushidadev@gmail.com>
2019-04-10 12:40:03 -0700

    Add more test cases

--- lab1_test_cases.py
+++ lab1_test_cases.py
@@ -9,15 +9,30 @@ class TestLab1(unittest.TestCase):
         tlist = None
         with self.assertRaises(ValueError):  # used to check for exception
             max_list_iter(tlist)
+            
+        zerolist = []
+        self.assertEqual(max_list_iter(zerolist), None)
 
+        nlist = [3, 4, 5, 2, 4, 5, 6, 2]
+        self.assertEqual(max_list_iter(nlist), 6)
+        
     def test_reverse_rec(self):
         self.assertEqual(reverse_rec([1,2,3]),[3,2,1])
+        
+        with self.assertRaises(ValueError):
+            reverse_rec(None)
 
     def test_bin_search(self):
         list_val =[0,1,2,3,4,7,8,9,10]
         low = 0
         high = len(list_val)-1
         self.assertEqual(bin_search(4, 0, len(list_val)-1, list_val), 4 )
+
+        self.assertEqual(bin_search(5, 0, len(list_val)-1, list_val), None)
+
+        nlist = None
+        with self.assertRaises(ValueError):
+            bin_search(4, 0, 1, nlist)
 
 if __name__ == "__main__":
         unittest.main()
